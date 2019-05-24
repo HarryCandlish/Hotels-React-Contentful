756445479c4c8faa6ddb27a1c23c9732a56ddff8
HarryCandlish <hcnd90@gmail.com>
2019-05-24 15:48:07 +1200

    built nav, working on hero banner

--- hotels/src/App.js
+++ hotels/src/App.js
@@ -1,12 +1,25 @@
 import React from "react";
-
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
+
+import Home from "./pages/Home";
+import Rooms from "./pages/Rooms";
+import SingleRoom from "./pages/SingleRoom";
+import Error from "./pages/Error";
+
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div className="App">
-      <h1>hello </h1>
-    </div>
+    <Router>
+      <Navbar />
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/rooms" component={Rooms} />
+        <Route exact path="/rooms/:slug" component={SingleRoom} />
+        <Route component={Error} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ hotels/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Hero from "../components/Hero";
+
+const Home = () => {
+  return (
+    <div>
+      <Hero hero="defaultHero" />
+    </div>
+  );
+};
+
+Hero.defaultProps = {
+  hero: "defaultHero"
+};
+
+export default Home;
