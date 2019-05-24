cac8fd6639761f314810f9f562ae5144b6512d4a
HarryCandlish <hcnd90@gmail.com>
2019-05-24 16:24:35 +1200

    created services

---
+++ hotels/src/components/Services.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import { FaCocktail, FaHiking, FaShuttleVan, FaBeer } from "react-icons/fa";
+import Title from "./Title";
+
+export default class Services extends Component {
+  state = {
+    services: [
+      {
+        icon: <FaCocktail />,
+        title: "Free Cocktails",
+        info:
+          "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+      },
+      {
+        icon: <FaHiking />,
+        title: "Hiking",
+        info:
+          "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+      },
+      {
+        icon: <FaShuttleVan />,
+        title: "Free Shuttle",
+        info:
+          "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+      },
+      {
+        icon: <FaBeer />,
+        title: "Good Beer",
+        info:
+          "is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries"
+      }
+    ]
+  };
+  render() {
+    return (
+      <section className="services">
+        <Title title="services" />
+        <div className="services-center">
+          {this.state.services.map((item, index) => {
+            return (
+              <article key={index} className="service">
+                <span>{item.icon}</span>
+                <h6>{item.title}</h6>
+                <p>{item.info}</p>
+              </article>
+            );
+          })}
+        </div>
+      </section>
+    );
+  }
+}
